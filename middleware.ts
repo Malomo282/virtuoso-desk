9ee99b4e1a5425f7a9a01f854cd16d6600ceba42
Malomo282 <jesseappiah28@gmail.com>
2026-07-12 20:43:26 +0100

    Simplify middleware for Vercel

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 import{NextResponse}from'next/server'
 import type{NextRequest}from'next/server'
 export function middleware(request:NextRequest){
-const{pathname}=request.nextUrl
-const pub=['/login','/reset-password','/update-password','/auth/callback','/register']
-if(pub.some(r=>pathname.startsWith(r)))return NextResponse.next()
 return NextResponse.next()}
 export const config={matcher:['/((?!_next/static|_next/image|favicon.ico).*)']}
